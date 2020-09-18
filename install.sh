801bd17cf1953d0af6d836677ed6478025095cda
Jon Staab <shtaab@gmail.com>
2020-09-18 15:53:49 -0700

    Tweak installation script, rename tmux file

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-mkdir -p ~/.config/jstaab ~/.tmuxp ~/my
+mkdir -p ~/.config/kak ~/.tmuxp ~/my
 
-ln -f src/shell.sh ~/.config/jstaab/shell.sh
 ln -f src/kakrc ~/.config/kak/kakrc
 ln -f src/tmux.conf ~/.tmux.conf
 ln -f src/gitconfig ~/.gitconfig
-ln -f src/tmuxp/cc.yaml ~/.tmuxp/cc.yaml
+ln -f src/tmuxp/main.yaml ~/.tmuxp/main.yaml
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo 'export PATH=$PATH:'$DIR'/pyr' >> ~/.config/jstaab/path.sh
 echo "Manual steps:
 
 - Set up ngrok.yml
+- Set up path
 - Source ~/.jstaab.sh in zshrc/fishrc/bashrc"
