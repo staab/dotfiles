60899ac85371b6a4d449b9cc2776a58932cb904a
Jon Staab <shtaab@gmail.com>
2020-11-25 15:55:40 -0800

    Update tmuxp, kak

--- install.sh
+++ install.sh
@@ -12,5 +12,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Manual steps:
 
 - Set up ngrok.yml
-- Install jq fd-find kakoune postgresql postgresql-server xclip fzf ack fish
-- Install nvm pyenv pipenv tmuxp
+- Install jq fd-find kakoune postgresql postgresql-server xsel fzf ack fish
+          make zlib-devel bzip2-devel readline-devel openssl-devel libffi-devel
+          llvm ncurses-devel lzma-sdk-devel libyaml-devel redhat-rpm-config
+          sqlite-devel libpq-devel postgresql-contrib htop wkhtmltopdf entr
+- Install nvm pyenv pipenv tmuxp yarn
