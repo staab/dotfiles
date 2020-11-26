7a89d1a46c6f4b9a081959b5c4ca48e1a03d8d4e
Jon Staab <shtaab@gmail.com>
2020-11-25 17:53:33 -0800

    Add debian deps

--- install.sh
+++ install.sh
@@ -12,8 +12,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Manual steps:
 
 - Set up ngrok.yml
-- Install jq fd-find kakoune postgresql postgresql-server xsel fzf ack fish
+- Fedora: jq fd-find kakoune postgresql postgresql-server xsel fzf ack fish
           make zlib-devel bzip2-devel readline-devel openssl-devel libffi-devel
           llvm ncurses-devel lzma-sdk-devel libyaml-devel redhat-rpm-config
           sqlite-devel libpq-devel postgresql-contrib htop wkhtmltopdf entr
+- Debian: jq fd-find kakoune postgresql xsel fzf ack fish make libreadline-dev
+          libssl-dev libffi-dev llvm ncurses-dev libyaml-dev libsqlite-dev
+          libpq-dev postgresql-contrib htop wkhtmltopdf entr zlib1g-dev
+          libsqlite3-dev libbz2-dev
 - Install nvm pyenv pipenv tmuxp yarn
